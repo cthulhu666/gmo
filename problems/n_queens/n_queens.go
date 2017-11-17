96993d76fc312e574559ccf5d1ff1c16553c1c9d
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-11-17 02:02:26 +0100

    Develop n-queens problem

--- problems/n_queens/n_queens.go
+++ problems/n_queens/n_queens.go
@@ -3,16 +3,16 @@ package main
 import (
 	"math/rand"
 	gmo "github.com/cthulhu666/gmo/engine"
+	"fmt"
 )
 
 var rnd = rand.New(rand.NewSource(0))
 var populationSize = 40
 
 var checkboard_size = 8
-var n_queens = 8
 
 func main() {
-	problem := NQueensProblem{checkboard_size: checkboard_size, number_of_queens: n_queens}
+	problem := NQueensProblem{checkboard_size: checkboard_size, rnd: rnd}
 	selection := gmo.TournamentSelection{Size: 2, Rnd: rnd, Comparator: gmo.ObjectiveComparator}
 
 	//pmx, err := pmx(rnd); panicOnError(err)
@@ -31,14 +31,86 @@ func initialPopulation() []gmo.Solution {
 }
 
 type NQueensProblem struct {
-	checkboard_size int
-	number_of_queens int
+	rnd              *rand.Rand
+	checkboard_size  int
 }
 
-func (p NQueensProblem) Evaluate(solution Solution) Evaluation {
-	// TODO
+func (p NQueensProblem) Evaluate(solution gmo.Solution) gmo.Evaluation {
+	f := float64(solution.(board).rowClashes() + solution.(board).diagonalClashes())
+	return gmo.Evaluation{Objectives: []float64{f}}
 }
 
-func (p NQueensProblem) RandomSolution() Solution {
-	// TODO
+func (p NQueensProblem) RandomSolution() gmo.Solution {
+	var arr []int
+	for i := 0; i < p.checkboard_size; i++ {
+		arr = append(arr, p.rnd.Intn(checkboard_size))
+	}
+	return board{arr}
+}
+
+type board struct {
+	columns []int
+}
+
+func (b board) getColumns() []int {
+	return append([]int(nil), b.columns...)
+}
+
+func (b board) Id() string {
+	return "" // TODO
+}
+
+func (b board) Checksum() string {
+	return "" // TODO
+}
+
+func (b board) rowClashes() int {
+	m := make(map[int]int)
+	for _, row := range b.columns {
+		m[row]++
+	}
+	clashes := 0
+	for _, count := range m {
+		if count > 1 {
+			clashes += count - 1
+		}
+	}
+	return clashes
+}
+
+func (b board) diagonalClashes() int {
+	clashes := 0
+	d1 := mapWithIndex(b.columns, func(a, i int) int { return a + i })
+	d2 := mapWithIndex(b.columns, func(a, i int) int { return a - i })
+	fmt.Println(d1, d2)
+	m1 := count(d1)
+	m2 := count(d2)
+	fmt.Println(m1, m2)
+	for _, count := range m1 {
+		if count > 1 {
+			clashes += count - 1
+		}
+	}
+	for _, count := range m2 {
+		if count > 1 {
+			clashes += count - 1
+		}
+	}
+	return clashes
+}
+
+func mapWithIndex(arr []int, f func(a, i int) int) []int {
+	rs := make([]int, len(arr))
+	for i, a := range arr {
+		rs[i] = f(a, i)
+	}
+	return rs
+}
+
+func count(arr []int) map[int]int {
+	m := make(map[int]int)
+	for _, a := range arr {
+		m[a]++
+	}
+	return m
 }
---
+++ problems/n_queens/n_queens_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRandomSolution(t *testing.T) {
+	p := problem(t)
+	s := p.RandomSolution()
+	assert.Len(t, s.(board).getColumns(), 10)
+}
+
+
+func TestColumnClashes(t *testing.T) {
+	b := board{[]int{0, 1, 1, 2, 2, 3, 4, 7}}
+	assert.Equal(t, 2, b.rowClashes())
+}
+
+func TestDiagonalClashes(t *testing.T) {
+	b := board{[]int{0, 1, 4, 3, 7, 0, 5, 0}}
+	assert.Equal(t, 4, b.diagonalClashes())
+	b = board{[]int{0, 1, 0, 1, 1, 0, 0, 0}}
+	assert.Equal(t, 4, b.diagonalClashes())
+	b = board{[]int{0, 1, 0, 1, 0, 1, 0, 1}}
+	assert.Equal(t, 7, b.diagonalClashes())
+	b = board{[]int{0, 1, 2, 3, 4, 5, 6, 7}}
+	assert.Equal(t, 7, b.diagonalClashes())
+}
+
+func problem(t *testing.T) NQueensProblem {
+	t.Helper()
+	return NQueensProblem{checkboard_size: 10, rnd: rnd}
+}
+
+
+/*
+
+zmienić układ współrzędnych?
+
+7 ....Q...
+6 ........
+5 ......Q.
+4 ..Q.....
+3 ...Q....
+2 ........
+1 .Q......
+0 Q....Q.Q
+ */
+
